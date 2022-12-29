0e3ab95eac4e7120ddab32b2c797904fbbeb4532
aswanesher <aswansetiawan01@gmail.com>
2022-12-29 10:09:05 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,6 @@ use Illuminate\Support\Facades\Route;
 }); */
 
 Auth::routes();
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::middleware(['auth'])->group(function () {
     Route::prefix('backend')->group(function () {
